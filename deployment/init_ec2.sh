28f9923e1fdb10ac9ad1686491eb85758011f68b
FabianTraxler <fabi.traxler@hotmail.com>
2026-09-18 14:26:38 +0200

    Update version in init_ec2 script

--- deployment/init_ec2.sh
+++ deployment/init_ec2.sh
@@ -17,7 +17,7 @@ cd /home/ec2-user
 git clone https://github.com/FabianTraxler/decathlon_support_system.git
 cd decathlon_support_system
 # switch to deployment branch
-git checkout origin/release/v.1.1.0
+git checkout origin/release/1.2
 
 # go to deployment folder
 cd deployment
