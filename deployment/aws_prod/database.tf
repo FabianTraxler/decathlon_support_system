ee0d2b0bf924eec22db6f64123284c757ab180d4
FabianTraxler <fabi.traxler@hotmail.com>
2026-09-18 16:56:25 +0200

    Increase Prod resources and fix ec2 init script

--- deployment/aws_prod/database.tf
+++ deployment/aws_prod/database.tf
@@ -163,7 +163,7 @@ resource "aws_dynamodb_table" "time_group_store" {
   billing_mode   = "PROVISIONED"
   hash_key       = "name"
   read_capacity  = 3
-  write_capacity = 3
+  write_capacity = 5
 
   attribute {
     name = "name"
@@ -172,7 +172,7 @@ resource "aws_dynamodb_table" "time_group_store" {
 }
 
 resource "aws_appautoscaling_target" "time_group_store_read" {
-  max_capacity       = 10
+  max_capacity       = 15
   min_capacity       = 3
   resource_id        = "table/${aws_dynamodb_table.time_group_store.name}"
   scalable_dimension = "dynamodb:table:ReadCapacityUnits"
@@ -196,8 +196,8 @@ resource "aws_appautoscaling_policy" "time_group_store_read_policy" {
 }
 
 resource "aws_appautoscaling_target" "time_group_store_write" {
-  max_capacity       = 10
-  min_capacity       = 3
+  max_capacity       = 15
+  min_capacity       = 5
   resource_id        = "table/${aws_dynamodb_table.time_group_store.name}"
   scalable_dimension = "dynamodb:table:WriteCapacityUnits"
   service_namespace  = "dynamodb"
